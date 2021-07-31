956009ae1e34b67e252b987e917dc22aee9cdcea
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-07-31 20:48:37 +0900

    ad

--- face_recognition.py
+++ face_recognition.py
@@ -9,5 +9,13 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # face detection func
 def detect(gray, original): # params are gray image and original image
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # x, y, w, h
+    for (x, y, w, h) in (faces):
+        # call recognition func from opencv 
+        # first arg : image
+        # second arg: cordinate upper left angle
+        # third arg : low right left angle
+        # fourth arg : colour
+        # last arg : edges of rectangle
+        cv2.rectangle(original, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
