eb9309b0e0ace8f7c61098481962a0e243fb21f1
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-08-01 01:57:25 +0900

    f

--- face_recognition.py
+++ face_recognition.py
@@ -6,7 +6,6 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-
 # face detection func
 def detect(gray, original): # params are gray image and original image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # x, y, w, h
